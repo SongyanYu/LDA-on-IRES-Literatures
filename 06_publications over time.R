2d73641423ab31cc605b2517bcb86dfef9b7f127
Sunny Yu <s2974665@staff.ad.griffith.edu.au>
2022-07-29 11:17:50 +1000

    plot publication numbers over time

--- 06_publications over time.R
+++ 06_publications over time.R
@@ -4,19 +4,26 @@
 # Date created: 29/07/2022
 #---
 
-#---
-# 1. read in the second corpus
-#---
-papers<-readxl::read_xlsx("../../data/scopus_intermittent-streams_filtered.xlsx",
-                          sheet = "Sheet1")
-
 library(dplyr)
 library(ggplot2)
-papers %>%
-  group_by(Year) %>%
+library(viridis)   
+
+read.csv('../../R output/05_TopicDocsGamma.csv') %>%
+  rowwise() %>%
+  mutate(max.gamma = max(c_across(X1:X13)),
+         topic = as.character(match(max.gamma, c_across(X1:X13)))) %>%
+  group_by(Year, topic) %>%
   summarise(n = n()) %>%
   filter(Year <= 2021) %>%  # remove publications in 2022
-  ggplot() +
-  geom_line(aes(x = Year, y = n)) +
-  theme_classic() +
-  ylab('Number of publications')
+  mutate(topic = factor(topic, levels = c('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'))) %>%
+  ggplot(aes(x = Year, y = n, fill = topic)) +
+  geom_bar(stat = 'identity', position = 'stack') +
+  theme_bw() +
+  ylab('Number of publications') +
+  labs(fill = 'Topic') +
+  scale_fill_viridis(discrete = TRUE) +
+  theme(legend.position = c(0.1, 0.55))
+ggsave(filename = '../../Fig/06_publication over time.png',
+       width = 8, height = 5)
+
+
