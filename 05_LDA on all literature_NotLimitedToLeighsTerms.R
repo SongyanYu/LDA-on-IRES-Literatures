548a48761c45d2c17b0037523d87415498e9e3f7
Sunny Yu <s2974665@staff.ad.griffith.edu.au>
2022-08-09 16:03:59 +1000

    mute inset figure

--- 05_LDA on all literature_NotLimitedToLeighsTerms.R
+++ 05_LDA on all literature_NotLimitedToLeighsTerms.R
@@ -551,21 +551,21 @@ data.frame(slec.weight = colMeans(topic.weight)[1:13],
   annotate(geom = 'text', x = 0.066, y = 0.30, label = strsplit('1-Fish ecology/2-Water pollution/3-Hydrological modelling', split = '/'), size = 3)
 ggsave(filename = "../../Fig/05_TopicGenerality.png",width = 6,height = 4)
 
-data.frame(slec.weight = colMeans(topic.weight)[1:13],
-           unslec.weight = colMeans(topic.weight[14:26])) %>%
-  mutate(tp_name = topic.name.v) %>%
-  ggplot(aes(x = unslec.weight, y = slec.weight)) +
-  geom_text(aes(label = str_wrap(tp_name, 20)), position = position_jitter()) +
-  annotate(geom = "text", x = 0.056, y = 0.28, label = "Specific topics", size = 5, fontface = "italic") +
-  annotate(geom = "text", x = 0.065, y = 0.25, label = "General topics", size = 5, fontface = "italic") +
-  xlab("Mean weight (unselected articles)") +
-  ylab("Mean weight (selected articles)") +
-  theme_classic() +
+#data.frame(slec.weight = colMeans(topic.weight)[1:13],
+#           unslec.weight = colMeans(topic.weight[14:26])) %>%
+#  mutate(tp_name = topic.name.v) %>%
+#  ggplot(aes(x = unslec.weight, y = slec.weight)) +
+#  geom_text(aes(label = str_wrap(tp_name, 20)), position = position_jitter()) +
+#  annotate(geom = "text", x = 0.056, y = 0.28, label = "Specific topics", size = 5, fontface = "italic") +
+#  annotate(geom = "text", x = 0.065, y = 0.25, label = "General topics", size = 5, fontface = "italic") +
+#  xlab("Mean weight (unselected articles)") +
+#  ylab("Mean weight (selected articles)") +
+#  theme_classic() +
 #  xlim(c(0.0545, 0.0682)) +
 #  ylim(c(0.235, 0.305))
-  xlim(c(0.0545, 0.066)) +
-  ylim(c(0.259, 0.275))
-ggsave(filename = "../../Fig/05_TopicGenerality_inset.png",width = 5, height = 5)
+#  xlim(c(0.0545, 0.066)) +
+#  ylim(c(0.259, 0.275))
+#ggsave(filename = "../../Fig/05_TopicGenerality_inset.png",width = 5, height = 5)
 
 
 #---
