0cbaf321a887db677155964d04f894d2bf12fb1c
Sunny Yu <s2974665@staff.ad.griffith.edu.au>
2023-05-23 11:53:49 +1000

    retained vs. all

--- appendix_removed articles in Leigh.R
+++ appendix_removed articles in Leigh.R
@@ -2,15 +2,23 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-papers.abs<-papers[(papers$Abstract==""),]
+papers<-read.csv("../../data/Literatures in Leigh et al 2016.csv",skip = 1)
+
+papers.abs<-papers[!(papers$Abstract==""),]
 
 table(papers.abs$Associated.search.es.)
 
-df <- data.frame(Assessment = 11/62,
-                 Biogeochemistry = (9+3)/62,
-                 Fish = (16+1)/62,
-                 Invertebrate = (22+3+1)/62) %>%
-  pivot_longer(cols = c(Assessment, Biogeochemistry, Fish, Invertebrate), values_to = 'removed')
+df <- data.frame(Assessment = (329+1+65+45+2+1+66+7+12)/1174,
+                 Biogeochemistry = (1+200+65+3+2+32+7+1)/1174,
+                 Fish = (174+45+3+2+21+12+1)/1174,
+                 Invertebrate = (1+215+66+32+7+21+12+1)/1174) %>%
+  pivot_longer(cols = c(Assessment, Biogeochemistry, Fish, Invertebrate), values_to = 'retained')
+
+#df <- data.frame(Assessment = 11/62,
+#                 Biogeochemistry = (9+3)/62,
+#                 Fish = (16+1)/62,
+#                 Invertebrate = (22+3+1)/62) %>%
+#  pivot_longer(cols = c(Assessment, Biogeochemistry, Fish, Invertebrate), values_to = 'removed')
 
 table(papers$Associated.search.es.)
 df.total <- data.frame(Assessment = (340+1+65+45+2+1+66+7+12)/1236,
@@ -28,5 +36,5 @@ df %>%
   xlab('Topics') +
   ylab('Proportion') +
   theme(legend.position = 'top')
-ggsave(filename = '../../Fig/appendix_removed artiles in Leigh.png',
+ggsave(filename = '../../Fig/appendix_retained artiles in Leigh.png',
        width = 5, height = 3)
