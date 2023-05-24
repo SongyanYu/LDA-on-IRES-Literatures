2996378ee95fc907f98988731d41dac33ce10873
SongyanYu <sunny.yu@griffith.edu.au>
2023-05-24 13:20:02 +1000

    [calculate] co-occurring key terms among topics

---
+++ appendix_co-occurring top words.R
@@ -0,0 +1,15 @@
+words <- c('speci', 'fish', 'habitat', 'structur', 'dryland', 'popul', 'nativ', 'abund', 'divers', 'genet',
+           'qualiti', 'sampl', 'concentr', 'pollut', 'sourc', 'site', 'salin', 'urban', 'lake', 'contamin',
+           'catchment', 'runoff', 'event', 'rainfal', 'simul', 'time', 'streamflow', 'observ', 'predict', 'durat',
+           'dam', 'riparian', 'reach', 'veget', 'site', 'forest', 'plant', 'reservoir', 'regul', 'cover',
+           'wadi', 'arid', 'estim', 'method', 'loss', 'base', 'flash', 'paramet', 'analysi', 'drainag',
+           'variabl', 'pattern', 'spatial', 'scale', 'dynam', 'variat', 'influence', 'network', 'factor', 'process',
+           'soil', 'watersh', 'transport', 'land', 'load', 'measur', 'eros', 'suspend', 'yield', 'contribut',
+           'organ', 'nutrient', 'spring', 'concentr', 'activ', 'doc', 'input', 'zone', 'hyporh', 'process',
+           'recharg', 'aquif', 'system', 'surfac', 'infiltr', 'wadi', 'hydraul', 'vallei', 'zone', 'rate',
+           'manag', 'regim', 'assess', 'ecolog', 'natur', 'indic', 'environment', 'ecosystem', 'develop', 'condit',
+           'bed', 'deposit', 'desert', 'process', 'sand', 'fluvial', 'rate', 'section', 'leaf', 'lower',
+           'climat', 'low', 'chang', 'period', 'streamflow', 'temperatur', 'resourc', 'precipit', 'decreas', 'annual',
+           'dry', 'commun', 'perenni', 'macroinvertebr', 'drought', 'aquat', 'temporari', 'invertebr', 'assemblag', 'site')
+
+table(words)
